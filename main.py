36391704d1ed0a384e88646179c053c6491ac9a3
Rohan Shiva <rhnshiva@gmail.com>
2020-08-28 09:49:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, join_room, send, leave_room, emit
 import requests
+from requests.exceptions import ConnectionError
 # b0qhpry9_6FoEBH1tGXEWrWkYZJgo3uSLuQv3uhwR - Project Key
 # b0qhpry9 - Project ID
 from deta import Deta
@@ -11,17 +12,37 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+endpoints = {"room": ["https://f82f2c67cbfc46aa8e43a89d705a0b0e.i.tgcloud.io:14240/gsqlserver/gsql/codecheck", "tigergraph", "Browser123"]}
+link = "https://71fa3d9ce51140189fbb9436b84f6fd6.i.tgcloud.io:14240/gsqlserver/gsql/codecheck"
+username = "tigergraph"
+password = "Browser123"
+
 @app.route('/')
 def sessions():
-    return render_template('session.html')
+    return render_template('index.html')
 
 
 @socketio.on('join')
 def join(data):
     room = data['room']
-    join_room(room)
     room = str(room)
-   
+    box_link = data["link"]
+    box_username = data["username"]
+    box_password = data["password"]
+    join_room(room)
+
+    if room not in endpoints:
+        endpoints[room] = [box_link, box_username, box_password]
+    # else:
+    #     if room not in endpoints:
+    #         endpoints[room] = [data['link'], data['username'], data['password']]
+    #         link = endpoints[room][0]
+    #         username = endpoints[room][1]
+    #         password = endpoints[room][2]
+    #     else:
+    #         link = endpoints[room][0]
+    #         username = endpoints[room][1]
+    #         password = endpoints[room][2]
 
 @socketio.on('leave')
 def on_leave(data):
@@ -30,15 +51,26 @@ def on_leave(data):
 
 @socketio.on("send message")
 def message(data):
+
+
+    # try:
+    print(link, username, password)
     room = data['room']
     code = {
-    "code": data['message'],
-    "graph": "MyGraph"
-    }
-    response = requests.post("CODCHECK_ENDPOINT", 
-                        json=code, auth=('USERNAME', 'PASSWORD'))
+            "code": data['message'],
+            "graph": "MyGraph"
+            }
+    box_link = endpoints[room][0]
+    box_username = endpoints[room][1]
+    box_password = endpoints[room][2]
+    response = requests.post(box_link, 
+                        json=code, auth=(box_username, box_password))
+        
     emit('broadcast message', data['message'], room=room)
     emit('broadcast errors', response.json(), room=room)
+    # except ConnectionError as e:
+    #     emit('error', "Please try again with a valid box credentials, or check if your box is up", room=room)
+
 
 
 
@@ -46,4 +78,4 @@ def message(data):
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 5000))
 
-    socketio.run(app, debug=True,  port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
