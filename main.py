2d8ee22aec63088e3e052fb56ecbd69f8d3fd4ad
Rohan Shiva <rhnshiva@gmail.com>
2020-09-01 10:54:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, join_room, send, leave_room, emit
 import requests
 from requests.exceptions import ConnectionError
-# b0qhpry9_6FoEBH1tGXEWrWkYZJgo3uSLuQv3uhwR - Project Key
-# b0qhpry9 - Project ID
+
 from deta import Deta
 import os
 import json
@@ -13,10 +12,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-endpoints = {"room": ["https://f82f2c67cbfc46aa8e43a89d705a0b0e.i.tgcloud.io:14240/gsqlserver/gsql/codecheck", "tigergraph", "Browser123"]}
-link = "https://f82f2c67cbfc46aa8e43a89d705a0b0e.i.tgcloud.io:14240/gsqlserver/gsql/codecheck"
+endpoints = {"room": ["https://hey.i.tgcloud.io:14240/gsqlserver/gsql/codecheck", "tigergraph", "Password"]}
+link = "hey"
 username = "tigergraph"
-password = "Browser123"
+password = "Password"
+graphname = "graphname"
 
 @app.route('/')
 def sessions():
@@ -30,20 +30,12 @@ def join(data):
     box_link = data["link"]
     box_username = data["username"]
     box_password = data["password"]
+    box_graphname = data["graphname"]
     join_room(room)
 
     if room not in endpoints:
-        endpoints[room] = [box_link, box_username, box_password]
-    # else:
-    #     if room not in endpoints:
-    #         endpoints[room] = [data['link'], data['username'], data['password']]
-    #         link = endpoints[room][0]
-    #         username = endpoints[room][1]
-    #         password = endpoints[room][2]
-    #     else:
-    #         link = endpoints[room][0]
-    #         username = endpoints[room][1]
-    #         password = endpoints[room][2]
+        endpoints[room] = [box_link, box_username, box_password, box_graphname]
+
 
 @socketio.on('leave')
 def on_leave(data):
@@ -59,7 +51,7 @@ def message(data):
     room = data['room']
     code = {
             "code": data['message'],
-            "graph": "MyGraph"
+            "graph": endpoints[room][3]
             }
     box_link = endpoints[room][0]
     box_username = endpoints[room][1]
@@ -79,4 +71,4 @@ def message(data):
 if __name__ == '__main__':
 #     port = int(os.environ.get('PORT', 5000))
 
-    app.run(debug=True)
+    socketio.run(app, debug=True)
